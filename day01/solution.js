87f95c711f717a2bee9db6720498acfec1a6319e
Michael Engard <michael@engard.me>
2023-12-02 19:14:32 -0500

    Started AoC 2023!

--- day01/solution.js
+++ day01/solution.js
@@ -1,22 +1,84 @@
-const fs = require('fs')
+const fs = require('fs');
 
-// Get the raw input from https://adventofcode.com/2022/day/1
+// Get the raw input from https://adventofcode.com/2023/day/1
 const input = fs.readFileSync('./day01/input.txt', { encoding: 'utf8', flag: 'r' } )
-// Kind of want to replace this with an async fetch request straight from AoC, but #lazy.
+.split("\n")
+.filter( line => line.length > 0 );
 
-const elves = input
-    .split("\n\n") // Treat two line breaks as a separator between each "elf."
-    .map( elf => { // Translate each elf’s entry from raw text to an object with actual numbers.
-        return elf
-            .split("\n") // Treat one line break as a separator between each food item.
-            .reduce( ( total, item ) => total + Number(item), 0 ) // Convert the separated strings to numbers and add ’em up.
-    })
-    .sort( (a,b) => b - a ) // Sort the results from highest to lowest.
+let solution1 = input
+.map( line => {
+    line = line.replaceAll( /[^\d]/g, '' );
+    let firstDigit = line.charAt(0);
+    let lastDigit  = line.charAt( line.length - 1 );
+    return Number( firstDigit + lastDigit );
+})
+.reduce( ( total, line ) => total + line, 0 );
 
-console.info( 'Part 1: ' + elves[0] ) // 70698
+console.info( 'Part 1: ' + solution1 ); // 54159
 
-const topThree = elves
-    .slice(0,3) // Take the top three elves.
-    .reduce( ( total, elf ) => total + elf, 0 ) // Add ’em up just like the food items.
+const digitMap = new Map([
+    [ '1',     '1' ],
+    [ '2',     '2' ],
+    [ '3',     '3' ],
+    [ '4',     '4' ],
+    [ '5',     '5' ],
+    [ '6',     '6' ],
+    [ '7',     '7' ],
+    [ '8',     '8' ],
+    [ '9',     '9' ],
+    [ 'one',   '1' ],
+    [ 'two',   '2' ],
+    [ 'three', '3' ],
+    [ 'four',  '4' ],
+    [ 'five',  '5' ],
+    [ 'six',   '6' ],
+    [ 'seven', '7' ],
+    [ 'eight', '8' ],
+    [ 'nine',  '9' ]
+]);
 
-console.info( 'Part 2: ' + topThree ) // 206643
+let solution2 = input
+.map( line => {
+
+    let firstDigit, lastDigit;
+    
+    for (let i = 0; i < line.length; i++) {
+
+        if ( !firstDigit ) {
+
+            let lineFromStart = line.substring(0, i + 1);
+            
+            for (const [ string, digit ] of digitMap) {
+                if ( lineFromStart.match(string) ) {
+                    firstDigit = digit;
+                    break;
+                }
+            }
+
+        }
+
+        if ( !lastDigit ) {
+
+            let lineFromEnd = line.substring( line.length - 1 - i );
+        
+            for (const [ string, digit ] of digitMap) {
+                if ( lineFromEnd.match(string) ) {
+                    lastDigit = digit;
+                    break;
+                }
+            }
+
+        }
+
+        if ( firstDigit && lastDigit ) break;
+
+    }
+
+    line = Number( firstDigit + lastDigit );
+
+    return line;
+
+})
+.reduce( ( total, line ) => total + line, 0 );
+
+console.info( 'Part 2: ' + solution2 ); // 53866
--- readme.md
+++ readme.md
@@ -1,7 +1,1 @@
-My solutions for [Advent of Code 2022](https://adventofcode.com/).
-
-Run any day’s solution using Node.js, e.g.:
-
-```js
-npm run day-01
-```
+My solutions for [Advent of Code 2023](https://adventofcode.com/).
